3c2fb8cd8cc441af587c0ec8833226ce7eacdc56
Gokul <55547509+gokul-krishna-r@users.noreply.github.com>
2023-03-14 23:44:28 +0530

    generalised

--- rename.py
+++ rename.py
@@ -3,7 +3,7 @@ import csv
 
 
 arr=[]
-with open('spacesummit_1.csv') as csv_file:
+with open('test.csv') as csv_file:  #csv file name
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	line_count = 0
 	for row in csv_reader:
@@ -11,13 +11,13 @@ with open('spacesummit_1.csv') as csv_file:
 		print(arr);
 		line_count += 1
 
-os.chdir("C:\\Users\\rahul\Documents\cognifox_works\Affinity\space_up\spaceup_certificates\prevent_1")
+os.chdir("") #file location: eg C:\\Users\\name\Documents\test
 print(os.getcwd())
 
 for count, f in enumerate(os.listdir()):
 	f_name, f_ext = os.path.splitext(f)
-	f_name = "certificate_" + arr[count+1]
-	oldname=f"Untitled-{count+1}.pdf";
+	f_name = "certificate_" + arr[count+1] #new name to be replaced with
+	oldname=f"Untitled-{count+1}.pdf";  #current file name
  
 	new_name = f'{f_name}{f_ext}'
 	os.rename(oldname, new_name)
